d9bdabc25df8af200a9a36b08086bf37583ddfd0
Kimiko Escovilla <78675012+kimikoesc@users.noreply.github.com>
2026-09-25 08:31:16 +0900

    Time: 3 ms (6.47%) | Memory: 58.5 MB (5.63%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -1,10 +1,12 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    const result: boolean[] = [];
+    // const result: boolean[] = [];
 
-    for (let i = 0; i < candies.length; i++) {
-        const totalCandies = candies[i] + extraCandies;
-        result.push(Math.max(...candies, totalCandies) === totalCandies)
-    }
+    // for (let i = 0; i < candies.length; i++) {
+    //     const totalCandies = candies[i] + extraCandies;
+    //     result.push(Math.max(...candies, totalCandies) === totalCandies)
+    // }
 
-    return result
+    // return result
+
+    return candies.map((c) => Math.max(...candies, c + extraCandies) === c + extraCandies)
 };
